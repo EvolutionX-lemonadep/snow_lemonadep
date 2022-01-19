108b8abec485d78c0f5c0c419dacb2244686441b
oddlyspaced <oddlyspaced1@gmail.com>
2022-01-20 00:15:11 +0530

    lemonadep: Build UDFPS Animations

--- evolution_lemonadep.mk
+++ evolution_lemonadep.mk
@@ -26,6 +26,7 @@ $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 EVO_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1440
+EXTRA_UDFPS_ANIMATIONS := true
 
 PRODUCT_NAME := evolution_lemonadep
 PRODUCT_DEVICE := lemonadep
